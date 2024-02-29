45c26f021ada92e265b3d6fe398ea473cc9ebb5c
lucaf24 <lucaf24@g.ucla.edu>
2024-02-29 12:05:37 -0800

    Forgot to commit delete account testing

--- server/backend.test.js
+++ server/backend.test.js
@@ -348,6 +348,34 @@ describe('Firebase route testing', () => {
         assert(res.body.length == 1);
         assert(JSON.stringify(res.body[0]) == JSON.stringify(stateObj));
     });
+
+    it('should delete delete user content when delete-account is caleld', async () => {
+        const authDeleteStub = sinon.stub();
+
+        const firebaseAuthStub = sinon.stub(firebase, 'auth').returns({
+            currentUser: {delete: authDeleteStub},
+        });
+
+        getStub.callsFake(() => ({
+            data: () => ({ savedUML: ['id1', 'id2'] })
+        }));
+
+        docStub.onCall(0).callsFake((uid) => {
+            return uid
+        });
+
+        docStub.callsFake((uml_id) => {
+            return uml_id
+        });
+
+        const res = await request(app).post('/delete-account').send(req).expect(200);
+
+        sinon.assert.calledOnce(authDeleteStub);
+        assert(deleteStub.getCall(0).args[0] == 'id1');
+        assert(deleteStub.getCall(1).args[0] == 'id2');
+        assert(deleteStub.getCall(2).args[0] == 'test-uid');
+
+    });
 });
 
 describe('PlantUML diagram fetching testing', () => {
