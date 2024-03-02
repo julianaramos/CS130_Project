dd19fad3e4394c79b6d8cf63c53db3c12416525e
Daniel Yoonseo Kim <yoonseo.mail@gmail.com>
2024-03-02 13:31:46 -0800

    Google Login and Signup
    This does not have any deleting Google account availabilities currently

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,6 +16,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import NavBar from './NavBar';
 import axios from 'axios';
+import firebase from '../firebase';
 
 const Login = () => {
     const { uid } = useSelector((state) => state.user);
@@ -23,6 +24,7 @@ const Login = () => {
     
     const [email,setEmail] = useState('');
     const [password,SetPassword] =useState('')
+    const [errorMessage, setErrorMessage] = useState('');
     const body = {email: email, password:password }
     const navigate = useNavigate()
 
@@ -40,6 +42,30 @@ const Login = () => {
             console.error('Error:', error);
         }
     }
+
+    const handleGoogleLogIn = async () => {
+        try {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            const result = await firebase.auth().signInWithPopup(provider);
+            const user = result.user;
+            const userId = user.uid;
+            // Check if user exists in your database
+            const res = await axios.post('http://localhost:4000/google-login', { user: user, uid: userId });
+            if (res.status === 200) {
+                dispatch(login(userId));
+                navigate('/');
+            }
+            console.log('Response data:', res);
+        } catch (error) {
+            if (error.response && error.response.status === 404 && error.response.data === "User does not exist") {
+                await firebase.auth().currentUser.delete();
+                setErrorMessage('User does not exist. Please sign up instead.');
+            } else {
+                setErrorMessage('An error occurred. Please try again.');
+            }
+        }
+    };
+
     // let x = <div> Not Logged In </div>
     // if (uid !== null){
     //     x = <div> Logged In </div>
@@ -116,6 +142,16 @@ const Login = () => {
                                 >
                                 Sign In
                                 </Button>
+                                <Button
+                                type="button"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 5, mb: 2}}
+                                onClick={handleGoogleLogIn}
+                                >
+                                Sign In with Google
+                                </Button>
+                                <p style={{color: 'red'}}>{errorMessage}</p>
                                 <Grid container>
                                 <Grid item>
                                     <Link to ="/signup" variant="body2">
